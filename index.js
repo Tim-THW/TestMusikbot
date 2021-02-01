c394e4b4e7ff08244d0c036b09006d907453a774
Tim-THW <77691248+Tim-THW@users.noreply.github.com>
2021-02-01 12:54:33 +0100

    V0.1

---
+++ index.js
@@ -0,0 +1,40 @@
+const {Client,} = require('discord.js');
+const { play, stop } = require('./command.js');
+const bot = new Client();
+
+
+bot.on('ready', () => console.log("On!"));
+
+bot.on('message', (msg)=>{
+      if(msg.author.bot) return;
+
+    const prefix = '?';
+    if (!msg.content.startsWith(prefix)) return;
+
+    const commandName = getCommandName(prefix, msg.content);
+    const args = getCommandArgs(prefix, msg.content);
+
+    if (commandName === 'play')
+    return play(msg, args);
+    else if (commandName ==='stop')
+    return stop(msg, args);
+
+
+
+
+});
+
+function getCommandName(prefix, content) {
+    return content
+    .slice(prefix.length)
+    .split(' ')[0];
+}
+
+function getCommandArgs(prefix, content) {
+    return content
+    .slice(prefix.length)
+    .split(' ')
+    .slice(1);
+}
+
+bot.login(process.env.token);
